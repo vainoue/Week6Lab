72cf1aca00bad7c5279024c89807d370f09b872d
vitor <vitor@LAPTOP-QS24IM11>
2023-02-14 00:29:07 -0700

    Week6Lab Initial setup

---
+++ src/java/servlets/ShoppingListServlet.java
@@ -0,0 +1,30 @@
+
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author vitor
+ */
+public class ShoppingListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}
---
+++ web/WEB-INF/shoppingList.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : shoppingList
+    Created on : Feb 14, 2023, 12:15:54 AM
+    Author     : vitor
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Shopping List</title>
+    </head>
+    <body>
+        <h1>Shopping List Page</h1>
+    </body>
+</html>
